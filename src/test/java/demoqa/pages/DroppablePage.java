86225f03e8ffde52116b70b88221495a3c38488b
Boris Pechersky <bpechersky@gmx.com>
2025-06-28 15:11:00 -0700

    refactored DroppableTest.java to use DroppablePage.java

--- src/test/java/demoqa/pages/DroppablePage.java
+++ src/test/java/demoqa/pages/DroppablePage.java
@@ -3,6 +3,12 @@ package demoqa.pages;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import com.codeborne.selenide.WebDriverRunner;
+import java.time.Duration;
+import static com.codeborne.selenide.Condition.visible;
+
 
 public class DroppablePage {
 
@@ -25,11 +31,23 @@ public class DroppablePage {
 
 
     public void performDragAndDrop() {
-        actions()
-                .clickAndHold(getDraggable())
-                .moveToElement(getDroppable())
+        getDraggable().scrollIntoView(true).shouldBe(visible);
+        getDroppable().scrollIntoView(true).shouldBe(visible);
+
+        WebElement source = getDraggable().toWebElement();
+        WebElement target = getDroppable().toWebElement();
+
+        int xOffset = target.getLocation().getX() - source.getLocation().getX() + 10;
+        int yOffset = target.getLocation().getY() - source.getLocation().getY() + 10;
+
+        Actions actions = new Actions(WebDriverRunner.getWebDriver());
+        actions.moveToElement(source)
+                .clickAndHold()
+                .moveByOffset(xOffset, yOffset)
+                .pause(Duration.ofMillis(500))
                 .release()
                 .perform();
     }
+
 
 }
--- src/test/java/demoqa/tests/DroppableTest.java
+++ src/test/java/demoqa/tests/DroppableTest.java
@@ -2,45 +2,43 @@ package demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import demoqa.TestBase;
+import demoqa.pages.DroppablePage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 
-public class DroppableTest {
+public class DroppableTest extends TestBase {
+
+    DroppablePage droppablePage = new DroppablePage();
 
     @Test
     public void testDragAndDropWithOffsets() {
         Configuration.browserSize = "1920x1080";
-        open("https://demoqa.com/droppable");
+
+        droppablePage.openPage();
 
         // Ensure elements are visible
-        $("#draggable").scrollIntoView(true).shouldBe(visible);
-        $("#droppable").scrollIntoView(true).shouldBe(visible);
+        droppablePage.getDraggable().scrollIntoView(true).shouldBe(visible);
+        droppablePage.getDroppable().scrollIntoView(true).shouldBe(visible);
 
-        WebElement source = $("#draggable").toWebElement();
-        WebElement target = $("#droppable").toWebElement();
+        WebElement source = droppablePage.getDraggable().toWebElement();
+        WebElement target = droppablePage.getDroppable().toWebElement();
 
-        // Get target location and use pixel offset
         int xOffset = target.getLocation().getX() - source.getLocation().getX() + 10;
         int yOffset = target.getLocation().getY() - source.getLocation().getY() + 10;
 
         Actions actions = new Actions(WebDriverRunner.getWebDriver());
-        actions
-                .moveToElement(source)
+        actions.moveToElement(source)
                 .clickAndHold()
                 .moveByOffset(xOffset, yOffset)
                 .pause(java.time.Duration.ofMillis(500))
                 .release()
                 .perform();
 
-        sleep(1000); // Allow DOM update
-
-        // Final assertion
-        $("#droppable p").shouldHave(text("Dropped!"));
+        droppablePage.getDropText().shouldHave(text("Dropped!"));
     }
 }
-
